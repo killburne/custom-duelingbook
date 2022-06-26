419d10a56202056c1c3cfaf6af00a4710774b06a
Amadej Kastelic <amadejkastelic7@gmail.com>
2022-06-26 12:57:08 +0200

    Custom sleeves for player, fix custom pfp for player

--- custom-duelingbook.user.js
+++ custom-duelingbook.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Custom DB
 // @description  Adds options to customize DB and make it more streamer friendly
-// @version      1.1.34
+// @version      1.1.35
 // @author       Killburne
 // @license		 MIT
 // @namespace    https://www.yugioh-api.com/
@@ -85,7 +85,13 @@
                 default: '🐔👍'
             },
             sleeveUrl: {
-                label: 'Sleeve image url',
+                label: 'Sleeve image url for others',
+                type: 'text',
+                size: 300,
+                default: 'https://www.duelingbook.com/./images/sleeves/1.jpg'
+            },
+            ownSleeveUrl: {
+                label: 'Sleeve image url for you',
                 type: 'text',
                 size: 300,
                 default: 'https://www.duelingbook.com/./images/sleeves/1.jpg'
@@ -1815,11 +1821,12 @@
     }
     function hideProfilePictures() {
         const pfpUrls = getConfigEntry('pfpUrls').split('\n').map(p => p.trim()).filter(p => !!p);
+        const ownPfpUrl = getConfigEntry('ownPfpUrl');
+        const bottomUsername = document.querySelector('#avatar1 .username_txt').textContent;
         //var bottomPfpUrls = GM_config.get('bottomPfpUrl').split('\n').map(p => p.trim()).filter(p => !!p);
 
         if (pfpUrls.length > 0) {
             const topUsername = document.querySelector('#avatar2 .username_txt').textContent;
-            const bottomUsername = document.querySelector('#avatar1 .username_txt').textContent;
 
             const topRandom = xmur3(topUsername);
             const pfpUrl = pfpUrls[topRandom() % pfpUrls.length];
@@ -1829,8 +1836,6 @@
                 }
             }
 
-            const ownPfpUrl = getConfigEntry('ownPfpUrl');
-
             if (bottomUsername != (window.unsafeWindow || window).user_username) {
                 const botRandom = xmur3(bottomUsername);
                 const bottomPfpUrl = pfpUrls[topRandom() % pfpUrls.length];
@@ -1839,11 +1844,12 @@
                         pfpBot.setAttribute('src', bottomPfpUrl);
                     }
                 }
-            } else if (ownPfpUrl) {
-                for (const pfpBot2 of document.querySelectorAll('#avatar1 .image')) {
-                    if (ownPfpUrl !== pfpBot2.getAttribute('src')) {
-                        pfpBot2.setAttribute('src', ownPfpUrl);
-                    }
+            }
+        }
+        if (ownPfpUrl && bottomUsername === (window.unsafeWindow || window).user_username) {
+            for (const pfpBot2 of document.querySelectorAll('#avatar1 .image')) {
+                if (ownPfpUrl !== pfpBot2.getAttribute('src')) {
+                    pfpBot2.setAttribute('src', ownPfpUrl);
                 }
             }
         }
@@ -2447,6 +2453,11 @@
             if (getConfigEntry('active') && sleeveUrl) {
                 data.player1.sleeve = sleeveUrl;
                 data.player2.sleeve = sleeveUrl;
+            }
+            const ownSleeveUrl = getConfigEntry('ownSleeveUrl');
+            const bottomUsername = document.querySelector('#avatar1 .username_txt').textContent;
+            if (getConfigEntry('active') && ownSleeveUrl && bottomUsername === (window.unsafeWindow || window).user_username) {
+                data.player1.sleeve = ownSleeveUrl;
             }
             originalInitPlayers(data);
         };
